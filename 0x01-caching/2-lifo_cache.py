8bc791dd8e1e112b020e351cfe2f427d51bc1fd4
ismael <spacingismael@gmail.com>
2024-10-29 20:52:52 +0100

    task 2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,2 +1,31 @@
 #!/usr/bin/env python3
-"""  """
+""" 2. LIFO Caching """
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """  inherits from BaseCaching and
+         is a caching system
+    """
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ assign to the dictionary
+        self.cache_data the item value for the key key"""
+        obj = self.cache_data
+        if key in obj:
+            del obj[key]
+        if len(obj) >= BaseCaching.MAX_ITEMS:
+            x = list(obj.keys())[-1]
+            del obj[x]
+            print('DISCARD: {}'.format(x))
+        if key and item:
+            obj[key] = item
+
+    def get(self, key):
+        """ return the value in self.cache_data
+        linked to key"""
+        return self.cache_data.get(key, None)
