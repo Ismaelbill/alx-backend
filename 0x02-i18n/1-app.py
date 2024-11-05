245d0ca76a8d441c6eae932438be63ff6a6d261b
ismael <spacingismael@gmail.com>
2024-11-05 15:46:07 +0100

    task 1 babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python3
+""" Basic Babel setup """
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """class - configures available languages & timezone"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def home():
+    """function returns a rendered template"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
